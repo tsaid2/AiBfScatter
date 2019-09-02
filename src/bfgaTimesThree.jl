3c8a8a31061946b22b572d8e975c4a414567cd65
tsaid2 <tuami.fatu@gmail.com>
2019-09-02 11:31:23 +0200

    .

--- src/bfgaTimesThree.jl
+++ src/bfgaTimesThree.jl
@@ -19,7 +19,7 @@ module bfgaTimesThree
     using Distributed
 
 
-    _trainingExamples = [ 4, 5, 8, 15 ]
+    _trainingExamples = [ 1, 4, 5, 8, 15 ]
     l_trainingExamples = length(_trainingExamples)
 
     function fitness(ent, instructionsSet)
